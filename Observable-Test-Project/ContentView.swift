ff8bd9c208acceb8dc6838abd4bfa193fa9b50db
Jacob Sock <jacobmsock@gmail.com>
2023-09-29 10:48:42 -0400

    Initial Commit

---
+++ Observable-Test-Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Observable-Test-Project
+//
+//  Created by Jacob M Sock  on 9/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Observable-Test-Project/Observable_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Observable_Test_ProjectApp.swift
+//  Observable-Test-Project
+//
+//  Created by Jacob M Sock  on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Observable_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
