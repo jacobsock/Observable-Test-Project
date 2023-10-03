84a9294a58fde786039878546f5c73d6a92e9637
Jacob Sock <jacobmsock@gmail.com>
2023-10-02 22:52:38 -0400

    add SpaceshipCategoryView, SpaceshipDetailView, and AddSpaceshipButtonView

---
+++ Observable-Test-Project/AddSpaceshipButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  AddSpaceshipButtonView.swift
+//  Observable-Test-Project
+//
+//  Created by Jacob M Sock  on 10/2/23.
+//
+
+import SwiftUI
+
+struct AddSpaceshipButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSpaceshipButtonView()
+}
---
+++ Observable-Test-Project/SpaceshipDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SpaceshipDetailView.swift
+//  Observable-Test-Project
+//
+//  Created by Jacob M Sock  on 10/2/23.
+//
+
+import SwiftUI
+
+struct SpaceshipDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpaceshipDetailView()
+}
